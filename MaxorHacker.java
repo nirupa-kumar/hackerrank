c8b86ceb8104211c2d294382bb5312a8abc12a07
nirupa-kumar <nirupa-kumar@users.noreply.github.com>
2015-05-12 14:25:19 -0700

    Create MaxorHacker.java
    Maximizing XOR - Hackerrank problem

---
+++ MaxorHacker.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class MaxorHacker {
+/*
+ * Complete the function below.
+ */
+
+    static int maxXor(int l, int r) {
+        int maxint = 0;
+			for (int i = l ; i <= r ; i ++)
+			{
+				for (int j = l ; j <= r ;j++)
+				{
+			    int xorop = i ^ j;
+				if (xorop > maxint)
+				maxint = xorop;
+				}
+			}
+		
+	return maxint	;	
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int res;
+        int _l;
+        _l = Integer.parseInt(in.nextLine());
+        
+        int _r;
+        _r = Integer.parseInt(in.nextLine());
+        
+        res = maxXor(_l, _r);
+        System.out.println(res);
+        
+    }
+}
